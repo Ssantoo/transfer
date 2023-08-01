efbbbaeb6d5e5da697c1c9a7ddaba1ed3dc64aa7
DESKTOP-RM1C3C1\82107 <nowing0108@kakao.com>
2023-08-01 23:52:03 +0900

    feat. vue에 다국어 적용

---
+++ front/transfer/src/view/test.vue
@@ -0,0 +1,41 @@
+<template>
+    <v-dialog
+      v-model="dialog"
+      persistent
+      max-width="580"
+    >
+      <v-card class="pa-3">
+        <v-card-title class="text-h5 mb-3">
+          {{ translations ? (translations['권한이 없습니다.'] || '권한이 없습니다.') : '권한이 없습니다.' }}
+        </v-card-title>
+        <v-card-text>
+          <v-row>
+            <v-col>
+              {{ translations ? (translations['관리자에게 권한을 문의 해 주시기 바랍니다.'] || '관리자에게 권한을 문의 해 주시기 바랍니다.') : '관리자에게 권한을 문의 해 주시기 바랍니다.' }}
+            </v-col>
+          </v-row>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+              outlined
+              elevation="0"
+          >
+            {{ translations ? (translations['확인'] || '확인') : '확인' }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </template>
+  
+  <script>
+  import { mapState } from 'vuex';
+  
+  export default {
+    computed: {
+      ...mapState('language', ['selectedLanguage', 'translations']),
+    },
+    
+  };
+  </script>
+  
